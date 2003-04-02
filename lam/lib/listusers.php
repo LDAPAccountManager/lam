84edb939c70d4fce54f1f12261fccb896cba9c22
dechutes <dechutes>
2003-04-02 21:42:51 +0000

    checkboxes added

--- lam/lib/listusers.php
+++ lam/lib/listusers.php
@@ -28,8 +28,12 @@ include_once("ldap.php");
 // class representing local user entry with attributes of ldap user entry
 include_once("userentry.php");
 
-echo "<link rel=\"stylesheet\" type=\"text/css\" href=\"../style/layout.css\" />";
+echo "<head>";
 
+echo "<link rel=\"stylesheet\" type=\"text/css\" href=\"../style/layout.css\" />";
+echo "<script src=\"./functions.js\" type=\"text/javascript\" language=\"javascript\"></script>";
+echo "</head>";
+echo "<body bgcolor=\"#F5F5F5\">";
 // config object should be in session!!!
 $config = new Config();
 $ldap = new Ldap($config);
@@ -45,18 +49,22 @@ echo "<table width=\"100%\">\n";
 
 // print attribute headers
 echo "<tr>";
+echo "<th class=\"userlist\"></th>";
 echo "<th class=\"userlist\">Vorname</th>";
 echo "<th class=\"userlist\">Nachname</th>";
 echo "<th class=\"userlist\">Uid</th>";
 echo "<th class=\"userlist\">Home Verzeichnis</th>";
 echo "</tr>";
-
+$row_number = 0;
 foreach ($user_dn_list as $user_dn) {
-  echo "<tr>\n";
+
+echo "<tr onmouseover=\"setPointer(this, " . $row_number . ", 'over', '#DDDDDD', '#CCFFCC', '#FFCC99');\" onmouseout=\"setPointer(this, " . $row_number . ", 'out', '#DDDDDD', '#CCFFCC', '#FFCC99');\" onmousedown=\"setPointer(this, " . $row_number . ", 'click', '#DDDDDD', '#CCFFCC', '#FFCC99');\">\n";
+$row_number++;
 
   $userentry = new UserEntry();
   $userentry = $ldap->getEntry ($user_dn, $userentry);
   $ldap->getEntry ($user_dn, $userentry);
+  echo ("<td class=\"userlist\">" . "<input type=\"checkbox\" name=\"..\"" . "</td>");
   echo ("<td class=\"userlist\">" . current ($userentry->getGivenName()) . "</td>");
   echo ("<td class=\"userlist\">" . current ($userentry->getSn()) . "</td>");    
   echo ("<td class=\"userlist\">" . current ($userentry->getUid()) . "</td>");    
@@ -64,6 +72,7 @@ foreach ($user_dn_list as $user_dn) {
   echo "</tr>\n";
 }
 echo "</table>";
+echo "</body>";
 
 $ldap->close();
 
