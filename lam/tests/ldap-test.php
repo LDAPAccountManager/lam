2869120fb935b113d66ed476dcabf9a3569344b0
Roland Gruber <post@rolandgruber.de>
2003-04-27 16:28:58 +0000

    test for ldap.inc

---
+++ lam/tests/ldap-test.php
@@ -0,0 +1,68 @@
+<?php
+/*
+$Id$
+
+  This code is part of LDAP Account Manager (http://www.sourceforge.net/projects/lam)
+  Copyright (C) 2003  Roland Gruber
+
+  This program is free software; you can redistribute it and/or modify
+  it under the terms of the GNU General Public License as published by
+  the Free Software Foundation; either version 2 of the License, or
+  (at your option) any later version.
+
+  This program is distributed in the hope that it will be useful,
+  but WITHOUT ANY WARRANTY; without even the implied warranty of
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+  GNU General Public License for more details.
+
+  You should have received a copy of the GNU General Public License
+  along with this program; if not, write to the Free Software
+  Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+
+	.
+*/
+include_once("../lib/config.inc");
+include_once("../lib/ldap.inc");
+
+if ($url && $user && $pass){
+	$config = new Config();
+	$ldap = new Ldap($config);
+	echo ("Trying to connect...");
+	if ($ldap->connect($user, $pass)) echo "ok";
+	echo "<br>Check if __sleep/__wakeup works";
+	$ldap->__sleep();
+	$ldap->__wakeup();
+	echo "<br>Closing connection";
+	$ldap->destroy();
+	echo "<br><br><br>If you do not see anny error messages all should be ok.";
+	exit;
+}
+else {
+	// generate 256 bit key and initialization vector for user/passwd-encryption
+	$key = mcrypt_create_iv(32, MCRYPT_DEV_RANDOM);
+	$iv = mcrypt_create_iv(32, MCRYPT_DEV_RANDOM);
+
+	// save both in cookie
+	setcookie("Key", base64_encode($key), 0, "/");
+	setcookie("IV", base64_encode($iv), 0, "/");
+}
+
+?>
+
+<html>
+	<head>
+		<title>
+		<?
+		echo ("Login");
+		?>
+		</title>
+	</head>
+	<body>
+		<form action="ldap-test.php" method="post">
+			<p align="center"><b>Server URL:</b> <input type="text" name="url" value="ldap://localhost"></p>
+			<p align="center"><b>Username:</b> <input type="text" name="user"></p>
+			<p align="center"><b>Password:</b> <input type="password" name="pass"></p>
+			<p align="center"><input type="submit" name="submit" value="Ok"></p>
+		</form>
+	</body>
+</html>
