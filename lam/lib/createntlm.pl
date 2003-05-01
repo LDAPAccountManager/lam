c52655a2ea94ead8ef8553b5dfd24b13d1775ca8
katagia <katagia>
2003-05-01 17:28:48 +0000

    Inserted deleted GPL-Licence

--- lam/lib/createntlm.pl
+++ lam/lib/createntlm.pl
@@ -1,6 +1,7 @@
 #! /usr/bin/perl
 # File Modified from Tilo Lutz, TiloLutz@gmx.de to fit better
 # in lam, LDAP Account Manager
+# added last two lines and changed from package to normal perl-programm
 
 #
 # Samba LM/NT Hash Generating Library.
@@ -10,6 +11,17 @@
 #    ( $lmhash, $nthash ) = ntlmgen($pass);
 # or
 #    ntlmgen $pass, $lmhash, $nthash;
+#
+#   This program is free software; you can redistribute it and/or modify
+#   it under the terms of the GNU General Public License as published by
+#   the Free Software Foundation; either version 2 of the License, or
+#   (at your option) any later version.
+#
+#   This program is distributed in the hope that it will be useful,
+#   but WITHOUT ANY WARRANTY; without even the implied warranty of
+#   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#   GNU General Public License for more details.
+#
 #
 # Copyright(C) 2001 Benjamin Kuit <bj@it.uts.edu.au>
 #
