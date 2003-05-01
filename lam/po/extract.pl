b7792ebbaa0e9f3d4481532f03253c24bdefc0ae
katagia <katagia>
2003-05-01 17:37:07 +0000

    Added GPL licence

--- lam/po/extract.pl
+++ lam/po/extract.pl
@@ -1,5 +1,15 @@
 #!/usr/bin/perl
 #
+#   This program is free software; you can redistribute it and/or modify
+#   it under the terms of the GNU General Public License as published by
+#   the Free Software Foundation; either version 2 of the License, or
+#   (at your option) any later version.
+#
+#   This program is distributed in the hope that it will be useful,
+#   but WITHOUT ANY WARRANTY; without even the implied warranty of
+#   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#   GNU General Public License for more details.
+#
 # $Horde: horde/po/extract.pl,v 1.6 2001/09/14 20:03:33 jon Exp $
 #
 # Perl script to extract strings from all the files and print
