221bcf0e7fcd6b9d9daa75c36e155089f1fe94df
dechutes <dechutes>
2003-05-05 23:06:12 +0000

    added filter for users (still buggy)

--- lam/templates/lists/listusers.php
+++ lam/templates/lists/listusers.php
@@ -44,21 +44,10 @@ if ($_POST['new_user'] || $_POST['del_user']){
 		exit;
 }
 
-
 echo "<html><head><title>listusers</title>\n";
 echo "<link rel=\"stylesheet\" type=\"text/css\" href=\"../../style/layout.css\">\n";
 echo "</head><body>\n";
 echo "<script src=\"../../lib/functions.js\" type=\"text/javascript\" language=\"javascript\"></script>\n";
-
-
-		putenv("LANG=de_DE");
-		putenv("LANGUAGE=de_DE");
-		setlocale(LC_MESSAGES, "de_DE");
-		setlocale(LC_ALL, "de_DE");
-		bindtextdomain("messages", "../locale");
-		textdomain("messages");
-
-
 
 // generate attribute-description table
 $attr_array;	// list of LDAP attributes to show
@@ -66,6 +55,7 @@ $desc_array;	// list of descriptions for the attributes
 $attr_string = $_SESSION["config"]->get_userlistAttributes();
 $temp_array = explode(";", $attr_string);
 $hash_table = $_SESSION["ldap"]->attributeUserArray();
+
 for ($i = 0; $i < sizeof($temp_array); $i++) {
 // if value is predifined, look up description in hash_table
 if (substr($temp_array[$i],0,1) == "#") {
@@ -81,8 +71,20 @@ else {
 }
 }
 
+// configure search filter
 // Users have the attribute "*"
-$filter = "(&(|(objectClass=posixAccount) (objectClass=sambaAccount)) (!(uid=*$)))";
+$filter = "(&(&(|(objectClass=posixAccount) (objectClass=sambaAccount)) (!(uid=*$)))";
+for ($k = 0; $k < sizeof($desc_array); $k++) {
+
+  if ($_POST["filter" . strtolower($attr_array[$k])])
+    $filter = $filter . "(" . strtolower($attr_array[$k]) . "=" .
+      $_POST["filter" . strtolower($attr_array[$k])] . ")";
+  else
+    $_POST["filter" . strtolower($attr_array[$k])] = "*";
+}
+$filter = $filter . ")";
+
+
 $attrs = $attr_array;
 $sr = @ldap_search($_SESSION["ldap"]->server(),
 	$_SESSION["config"]->get_UserSuffix(),
@@ -90,7 +92,9 @@ $sr = @ldap_search($_SESSION["ldap"]->server(),
 if ($sr) {
 	$info = ldap_get_entries($_SESSION["ldap"]->server, $sr);
 	ldap_free_result($sr);
-	if ($info["count"] == 0) echo ("<br><br><font color=\"red\"><b>" . _("No Users found!") . "</b></font><br><br>");
+	if ($info["count"] == 0) echo ("<br><br><font color=\"red\"><b>" . 
+				       _("No Users found!") . 
+				       "</b></font><br><br>");
 }
 else echo ("<br><br><font color=\"red\"><b>" . _("LDAP Search failed! Please check your preferences. <br> No Users found!") . "</b></font><br><br>");
 
@@ -106,50 +110,65 @@ echo "<table rules=\"all\" class=\"userlist\" width=\"100%\">\n";
 echo "<tr class=\"userlist_head\"><th width=22 height=34></th><th></th>";
 // table header
 for ($k = 0; $k < sizeof($desc_array); $k++) {
-	echo "<th><a class=\"userlist\" href=\"listusers.php?list=" . strtolower($attr_array[$k]) . "\">" . $desc_array[$k] . "</a></th>";
+	echo "<th><a class=\"userlist\" href=\"listusers.php?list=" . 
+	  strtolower($attr_array[$k]) . "\">" . 
+	  $desc_array[$k] . "</a></th>";
+}
+echo "</tr>\n";
+echo "<tr class=\"test\"><th width=22 height=34></th><th>";
+echo "<input type=\"submit\" name=\"apply_filter\" value=\"" . _("Apply") . "\">";
+echo "</th>";
+// table header
+for ($k = 0; $k < sizeof($desc_array); $k++) {
+  echo "<th>";
+  echo ("<input type=\"text\" name=\"filter" . strtolower ($attr_array[$k]) . 
+	"\" value=\"" . $_POST["filter" . strtolower($attr_array[$k])] . "\">");
+  echo "</th>";
 }
 echo "</tr>\n";
 
 // print user list
 for ($i = 0; $i < sizeof($info); $i++) { // ignore last entry in array which is "count"
-	echo("<tr class=\"userlist\" onMouseOver=\"user_over(this, '" . $info[$i]["dn"] . "')\"" .
-								" onMouseOut=\"user_out(this, '" . $info[$i]["dn"] . "')\"" .
-								" onClick=\"user_click(this, '" . $info[$i]["dn"] . "')\"" .
-								" onDblClick=parent.frames[1].location.href=\"../account.php?type=user&DN='" . $info[$i]["dn"] . "'\">" .
-								" <td height=22><input onClick=\"user_click(this, '" . $info[$i]["dn"] . "')\" type=\"checkbox\" name=\"" . $info[$i]["dn"] . "\"></td>" .
-								" <td align='center'><a href=\"../account.php?type=user&DN='" . $info[$i]["dn"] . "'\">" . _("Edit") . "</a></td>");
-	for ($k = 0; $k < sizeof($attr_array); $k++) {
-		echo ("<td>");
-		// print all attribute entries seperated by "; "
-		if (sizeof($info[$i][strtolower($attr_array[$k])]) > 0) {
-			// delete first array entry which is "count"
-			array_shift($info[$i][strtolower($attr_array[$k])]);
-			// generate links for user members
-			if (strtolower($attr_array[$k]) == "memberuid") {
-				$linklist = array();
-				for ($d = 0; $d < sizeof($info[$i][strtolower($attr_array[$k])]); $d++) {
-					$user = $info[$i][strtolower($attr_array[$k])][$d]; // user name
-					$dn = $_SESSION["ldap"]->search_username($user); // DN entry
-					// if user was found in LDAP make link, otherwise just print name
-					if ($dn) {
-						$linklist[$d] = "<a href=../account.php?type=user&DN=\"" . $dn . "\" >" .
-										$info[$i][strtolower($attr_array[$k])][$d] . "</a>";
-					}
-					else $linklist[$d] = $user;
-				}
-				echo implode("; ", $linklist);
-			}
-			// print all other attributes
-			else {
-				echo implode("; ", $info[$i][strtolower($attr_array[$k])]);
-			}
-		}
-		echo ("</td>");
+  echo("<tr class=\"userlist\" onMouseOver=\"user_over(this, '" . $info[$i]["dn"] . "')\"" .
+       " onMouseOut=\"user_out(this, '" . $info[$i]["dn"] . "')\"" .
+       " onClick=\"user_click(this, '" . $info[$i]["dn"] . "')\"" .
+       " onDblClick=parent.frames[1].location.href=\"../account.php?type=user&DN='" . $info[$i]["dn"] . "'\">" .
+       " <td height=22><input onClick=\"user_click(this, '" . $info[$i]["dn"] . "')\" type=\"checkbox\" name=\"" . $info[$i]["dn"] . "\"></td>" .
+       " <td align='center'><a href=\"../account.php?type=user&DN='" . $info[$i]["dn"] . "'\">" . _("Edit") . "</a></td>");
+  for ($k = 0; $k < sizeof($attr_array); $k++) {
+    echo ("<td>");
+    // print all attribute entries seperated by "; "
+    if (sizeof($info[$i][strtolower($attr_array[$k])]) > 0) {
+      // delete first array entry which is "count"
+      array_shift($info[$i][strtolower($attr_array[$k])]);
+      // generate links for user members
+      if (strtolower($attr_array[$k]) == "memberuid") {
+	$linklist = array();
+	for ($d = 0; $d < sizeof($info[$i][strtolower($attr_array[$k])]); $d++) {
+	  $user = $info[$i][strtolower($attr_array[$k])][$d]; // user name
+	  $dn = $_SESSION["ldap"]->search_username($user); // DN entry
+	  // if user was found in LDAP make link, otherwise just print name
+	  if ($dn) {
+	    $linklist[$d] = "<a href=../account.php?type=user&DN=\"" . $dn . "\" >" .
+	      $info[$i][strtolower($attr_array[$k])][$d] . "</a>";
+	  }
+	  else $linklist[$d] = $user;
 	}
-	echo("</tr>\n");
+	echo implode("; ", $linklist);
+      }
+      // print all other attributes
+      else {
+	echo implode("; ", $info[$i][strtolower($attr_array[$k])]);
+      }
+    }
+    echo ("</td>");
+  }
+  echo("</tr>\n");
 }
 echo ("</table>");
-echo ("<p>&nbsp</p>\n");
+
+echo ("<br />");
+
 echo ("<table align=\"left\" border=\"0\">");
 echo ("<tr><td align=\"left\"><input type=\"submit\" name=\"new_user\" value=\"" . _("New User") . "\"></td>");
 echo ("<td align=\"left\"><input type=\"submit\" name=\"del_user\" value=\"" . _("Delete User(s)") . "\"></td></tr>");
@@ -161,14 +180,14 @@ echo "</body></html>\n";
 // rows are sorted with the first attribute entry of the sort column
 // if objects have attributes with multiple values the others are ignored
 function cmp_array($a, $b) {
-	// list specifies the sort column
-	global $list;
-	global $attr_array;
-	// sort by first attribute with name $list
-	if (!$list) $list = strtolower($attr_array[0]);
-	if ($a[$list][0] == $b[$list][0]) return 0;
-	else if ($a[$list][0] == max($a[$list][0], $b[$list][0])) return 1;
-	else return -1;
+  // list specifies the sort column
+  global $list;
+  global $attr_array;
+  // sort by first attribute with name $list
+  if (!$list) $list = strtolower($attr_array[0]);
+  if ($a[$list][0] == $b[$list][0]) return 0;
+  else if ($a[$list][0] == max($a[$list][0], $b[$list][0])) return 1;
+  else return -1;
 }
 
 ?>
