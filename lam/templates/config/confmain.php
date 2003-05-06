5b11f1d8470e90abb81c500f801e834acffaafd7
dechutes <dechutes>
2003-05-06 23:52:21 +0000

    changed structure and layout of page tried to add a new config variable called maxlistentries

--- lam/templates/config/confmain.php
+++ lam/templates/config/confmain.php
@@ -50,10 +50,13 @@ if (!(($conf->get_Passwd()) == $passwd)) {
 echo ("<html>\n");
 echo ("<head>\n");
 echo ("<title>" . _("LDAP Account Manager Configuration") . "</title>\n");
+echo "<link rel=\"stylesheet\" type=\"text/css\" href=\"../../style/layout.css\">\n";
 echo ("</head>\n");
 echo ("<body>\n");
 echo ("<p align=\"center\"><a href=\"http://lam.sf.net\" target=\"new_window\"><img src=\"../../graphics/banner.jpg\" border=1 alt=\"LDAP Account Manager\"></a></p><hr><br><br>\n");
 echo ("<form action=\"confsave.php\" method=\"post\">\n");
+
+echo ("<fieldset><legend><b>" . _("Server settings") . "</b></legend>");
 echo ("<table align=\"center\" border=\"0\">");
 echo ("<tr><td width=\"20%\"><p align=\"right\"><b>" . _("Server Address") . ": </b></p></td> <td width=\"30%\"><p align=\"left\"><input size=50 type=\"text\" name=\"serverurl\" value=\"" . $conf->get_ServerURL() . "\"></td>\n");
 echo ("<td width=\"50%\"><p align=\"left\">" . _("Server Address of LDAP server (e.g. ldap://localhost:389 or ldaps://localhost:636)") . "</p></td></tr>\n");
@@ -64,6 +67,12 @@ echo ("<tr><td><p align=\"right\"><b>" . _("GroupSuffix") . ": </b></p></td> <td
 echo ("<td><p align=\"left\">" . _("This is the suffix from where to search for groups.<br>(e.g. ou=group,dc=yourcompany,dc=com)") . "</p></td></tr>\n");
 echo ("<tr><td><p align=\"right\"><b>" . _("HostSuffix") . ": </b></p></td> <td><input size=50 type=\"text\" name=\"suffhosts\" value=\"" . $conf->get_HostSuffix() . "\"></td>\n");
 echo ("<td><p align=\"left\">" . _("This is the suffix from where to search for Samba hosts.<br>(e.g. ou=machines,dc=yourcompany,dc=com)") . "</p></td></tr>\n");
+echo ("</table>");
+echo ("</fieldset>");
+echo ("<br />");
+
+echo ("<fieldset><legend><b>" . _("Ranges") . "</b></legend>");
+echo ("<table align=\"center\" border=\"0\">");
 echo ("<tr><td>&nbsp</td></tr>");
 echo ("<tr><td align=\"right\"><b>" . _("Minimum UID number") . ": </b></td> <td align=\"left\"><input size=6 type=\"text\" name=\"minUID\" value=\"" . $conf->get_minUID() . "\">\n");
 echo ("&nbsp <b>" . _("Maximum UID number") . ": </b><input size=6 type=\"text\" name=\"maxUID\" value=\"" . $conf->get_maxUID() . "\"></td>\n");
@@ -74,6 +83,12 @@ echo ("<td><p align=\"left\">" . _("These are the minimum and maximum numbers to
 echo ("<tr><td align=\"right\"><b>" . _("Minimum Machine number") . ": </b></td> <td align=\"left\"><input size=6 type=\"text\" name=\"minMach\" value=\"" . $conf->get_minMachine() . "\">\n");
 echo ("&nbsp <b>" . _("Maximum Machine number") . ": </b><input size=6 type=\"text\" name=\"maxMach\" value=\"" . $conf->get_maxMachine() . "\"></td>\n");
 echo ("<td><p align=\"left\">" . _("These are the minimum and maximum numbers to use for Samba hosts. <br> Do not use the same range as for user IDs.") . "</p></td></tr>\n");
+echo ("</table>");
+echo ("</fieldset>");
+echo ("<br />");
+
+echo ("<fieldset><legend><b>" . _("LDAP List settings") . "</b></legend>");
+echo ("<table align=\"center\" border=\"0\">");
 echo ("<tr><td>&nbsp</td></tr>");
 echo ("<tr><td><p align=\"right\"><b>" . _("Attributes in User List:") . "</b></p></td><td><input size=50 type=\"text\" name=\"usrlstattr\" value=\"" . $conf->get_userlistAttributes() . "\"></td>");
 echo ("<td rowspan=3><p>" . _("This is the list of attributes to show in the lists. The entries can either be predefined values (e.g. '#cn' or '#gidNumber') or individual ones (e.g. 'cn:Group Name'). The entries are seperated by semicolons.")
@@ -84,18 +99,25 @@ echo ("<tr><td>&nbsp</td></tr>");
 echo ("<tr><td>&nbsp</td></tr>");
 echo ("<tr><td><p align=\"right\"><b>" . _("List of valid users") . ": </b></p></td> <td><input size=50 type=\"text\" name=\"admins\" value=\"" . $conf->get_Adminstring() . "\"></td>\n");
 echo ("<td><p align=\"left\">" . _("Usernames must be seperated by semicolons<br>(e.g. cn=admin,dc=yourcompany,dc=com;uid=root,ou=people,dc=yourcompany,dc=com)") . "</p></td></tr>\n");
-echo ("</table>\n");
-echo ("<p>&nbsp</p>\n");
+echo ("<tr><td><p align=\"right\"><b>" . _("Maximum displayed entries") . ": </b></p></td> <td><input size=50 type=\"text\" name=\"maxlistentries\" value=\"" . $conf->get_MaxListEntries() . "\"></td>\n");
+echo ("<td><p align=\"left\">" . _("This is the maximum count of entries which are displayed on one page of the user/host/group lists.") . "</p></td></tr>\n");
+echo ("</table>");
+echo ("</fieldset>");
+echo ("<br />");
+
+echo ("<fieldset><legend><b>" . _("Security settings") . "</b></legend>");
 echo ("<table align=\"left\" border=\"0\">");
 echo ("<tr><td bgcolor=\"red\" align=\"right\"><b>" . _("New Password") . ": </b></td> <td bgcolor=\"red\" align=\"left\"><input type=\"password\" name=\"pass1\"></td></tr>");
 echo ("<tr><td bgcolor=\"red\" align=\"right\"><b>" . _("Reenter Password") . ": </b></td> <td bgcolor=\"red\" align=\"left\"><input type=\"password\" name=\"pass2\"></td></tr>\n");
 echo ("</table>\n");
-echo ("<p>&nbsp</p>\n");
-echo ("<p>&nbsp</p>\n");
+echo ("</fieldset>");
+echo ("<br />");
+
 echo ("<table align=\"left\" border=\"0\">");
 echo ("<tr><td align=\"left\"><pre><input type=\"submit\" name=\"submitconf\" value=\"" . _("Submit") . "\">     <input type=\"reset\" name=\"resetconf\" value=\"" . _("Reset") . "\">");
 echo ("<input type=\"button\" name=\"back\" value=\"" . _("Abort") . "\" onClick=\"self.location.href='../login.php'\"></pre></td></tr>\n");
 echo ("</table>\n");
+
 echo ("<input type=\"hidden\" name=\"passwd\" value=\"" . $passwd . "\"><br>\n");
 echo ("</form>\n");
 echo ("</body>\n");
