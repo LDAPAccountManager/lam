145c71e18be8ae3a11fa7b3ae2a3821208af865d
Roland Gruber <post@rolandgruber.de>
2003-06-01 16:35:02 +0000

    fixed HTML

--- lam/templates/main_header.php
+++ lam/templates/main_header.php
@@ -19,27 +19,31 @@ $Id$
   along with this program; if not, write to the Free Software
   Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
 
-  
-  
+
+
 */
+
+echo ("<?xml version=\"1.0\" encoding=\"UTF-8\"?>\n");
+echo ("<!DOCTYPE HTML PUBLIC \"-//W3C//DTD HTML 4.01 Transitional//EN\" \"http://www.w3.org/TR/html4/loose.dtd\">\n");
 
 ?>
 
 <html>
-<head>
-<link rel="stylesheet" type="text/css" href="../style/layout.css" />
 
+<head>
+	<title></title>
+	<link rel="stylesheet" type="text/css" href="../style/layout.css">
 </head>
+
 <body>
 <table border=0 width="100%">
 	<tr>
-		<td width="100" rowspan=4>&nbsp</td>
-		<td rowspan=4>
-			<p align="center"><a href="http://lam.sf.net" target="new_window"><img src="../graphics/banner.jpg" border=1></a></p>
+		<td width="100" rowspan=3>&nbsp</td>
+		<td rowspan=3 align="center">
+			<a href="http://lam.sf.net" target="new_window"><img src="../graphics/banner.jpg" border=1 alt="LDAP Account Manager"></a>
 		</td>
-		<td width="100"><p align="right"><a href="./logout.php" target="_top"><? echo _("Logout") ?></a></p></td>
+	<td width="100" align="right" height=20><a href="./logout.php" target="_top"><b><? echo _("Logout") ?></b></a></td>
 	</tr>
-	<tr><td height=3></td></tr>
 	<tr>
     	<td align="right"><a href="./profedit/profilemain.php" target="mainpart"><? echo _("Profile Editor") ?></a></td>
 	</tr>
@@ -50,9 +54,10 @@ $Id$
 <br>
 <table border=0 align="center" width="600">
   <tr>
-    <td width="200"><p align="center"><a href="./lists/listusers.php" target="mainpart"> <? echo _("Users");?> </a></p></td>
-    <td width="200"><p align="center"><a href="./lists/listgroups.php" target="mainpart"> <? echo _("Groups");?> </a></p></td>
-    <td width="200"><p align="center"><a href="./lists/listhosts.php" target="mainpart"> <? echo _("Hosts");?> </a></p></td>
+    <td width="200" align="center"><a href="./lists/listusers.php" target="mainpart"> <? echo _("Users");?> </a></td>
+    <td width="200" align="center"><a href="./lists/listgroups.php" target="mainpart"> <? echo _("Groups");?> </a></td>
+    <td width="200" align="center"><a href="./lists/listhosts.php" target="mainpart"> <? echo _("Hosts");?> </a></td>
   </tr>
 </table>
 </body>
+</html>
