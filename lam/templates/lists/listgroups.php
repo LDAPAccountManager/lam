9a85b1c3eacd14432957ce77f955a72f2cc05921
Roland Gruber <post@rolandgruber.de>
2003-06-08 17:58:19 +0000

    fixed HTML errors

--- lam/templates/lists/listgroups.php
+++ lam/templates/lists/listgroups.php
@@ -170,7 +170,7 @@ for ($i = $table_begin; $i < $table_end; $i++) {
 	echo("<tr class=\"grouplist\" onMouseOver=\"group_over(this, '" . $info[$i]["dn"] . "')\"" .
 								" onMouseOut=\"group_out(this, '" . $info[$i]["dn"] . "')\"" .
 								" onClick=\"group_click(this, '" . $info[$i]["dn"] . "')\"" .
-								" onDblClick=parent.frames[1].location.href=\"../account.php?type=group&amp;DN='" . $info[$i]["dn"] . "'\">" .
+								" onDblClick=\"parent.frames[1].location.href='../account.php?type=group&amp;DN=" . $info[$i]["dn"] . "'\">" .
 								" <td height=22><input onClick=\"group_click(this, '" . $info[$i]["dn"] . "')\" type=\"checkbox\" name=\"" . $info[$i]["dn"] . "\"></td>" .
 								" <td align='center'><a href=\"../account.php?type=group&amp;DN='" . $info[$i]["dn"] . "'\">" . _("Edit") . "</a></td>");
 	for ($k = 0; $k < sizeof($attr_array); $k++) {
