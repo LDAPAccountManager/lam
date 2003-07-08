f292691548473833d92764e8e76dd1c609756843
Roland Gruber <post@rolandgruber.de>
2003-07-08 08:54:34 +0000

    updates for better translation

--- lam/templates/lists/listusers.php
+++ lam/templates/lists/listusers.php
@@ -9,12 +9,12 @@ $Id$
   it under the terms of the GNU General Public License as published by
   the Free Software Foundation; either version 2 of the License, or
   (at your option) any later version.
-  
+
   This program is distributed in the hope that it will be useful,
   but WITHOUT ANY WARRANTY; without even the implied warranty of
   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
   GNU General Public License for more detaexils.
-  
+
   You should have received a copy of the GNU General Public License
   along with this program; if not, write to the Free Software
   Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
@@ -38,8 +38,7 @@ $_POST = $_POST + $_GET;
 
 // insert header for html file
 echo "<?xml version=\"1.0\" encoding=\"ISO-8859-15\"?>\n";
-echo "<!DOCTYPE HTML PUBLIC \"-//W3C//DTD HTML 4.01 Transitional//EN\"
-\"http://www.w3.org/TR/html4/loose.dtd\">\n";
+echo "<!DOCTYPE HTML PUBLIC \"-//W3C//DTD HTML 4.01 Transitional//EN\" \"http://www.w3.org/TR/html4/loose.dtd\">\n";
 
 $usr_units = $_SESSION['usr_units'];
 session_register('usr_units');
