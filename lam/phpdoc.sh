7bf5ebfa108aebe03f6167c0f7b52055e41b5aa0
Roland Gruber <post@rolandgruber.de>
2008-11-02 17:47:17 +0000

    main is default package

--- lam/phpdoc.sh
+++ lam/phpdoc.sh
@@ -2,5 +2,5 @@
 
 rm -rf docs/devel/phpdoc
 mkdir docs/devel/phpdoc
-phpdoc -ue on --output "HTML:Smarty:PHP" -d ./ -t docs/devel/phpdoc -ti "LDAP Account Manager - Documentation" -dc "LDAP Account Manager"
+phpdoc -ue on --output "HTML:Smarty:PHP" -d ./ -t docs/devel/phpdoc -ti "LDAP Account Manager - Documentation" -dc "LDAP Account Manager" -dn main
  
