ad8dc17265a06f2e77cd5c7ae00d7b3e388b8307
Roland Gruber <post@rolandgruber.de>
2011-06-26 10:44:28 +0000

    new PLA release

---
+++ lam/templates/3rdParty/pla/lib/GidAttribute.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * Classes and functions for the template engine.
+ *
+ * @author The phpLDAPadmin development team
+ * @package phpLDAPadmin
+ */
+
+/**
+ * Represents a 'gidNumber' attribute
+ *
+ * @package phpLDAPadmin
+ * @subpackage Templates
+ */
+class GidAttribute extends Attribute {
+}
+?>
