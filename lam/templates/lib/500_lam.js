948da57ad37e467b319868198ed892025de5c1bf
Roland Gruber <post@rolandgruber.de>
2012-01-14 13:22:55 +0000

    fixed confirmation dialog

--- lam/templates/lib/500_lam.js
+++ lam/templates/lib/500_lam.js
@@ -3,7 +3,7 @@
 $Id$
 
   This code is part of LDAP Account Manager (http://www.ldap-account-manager.org/)
-  Copyright (C) 2003 - 2011  Roland Gruber
+  Copyright (C) 2003 - 2012  Roland Gruber
 
   This program is free software; you can redistribute it and/or modify
   it under the terms of the GNU General Public License as published by
@@ -309,6 +309,8 @@ function showConfirmationDialog(title, okText, cancelText, dialogDiv, formName) 
 		buttons: buttonList,
 		width: 'auto'
 	});
+	/* reattach dialog to form */
+	jQuery('#' + dialogDiv).parent().appendTo(document.forms[formName]);
 }
 
 
