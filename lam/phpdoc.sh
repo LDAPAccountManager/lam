86746d4070ea20dcf5aafcf6dc7a9e6f9f8148a6
Roland Gruber <post@rolandgruber.de>
2012-05-20 09:53:15 +0000

    new PHPDoc

--- lam/phpdoc.sh
+++ lam/phpdoc.sh
@@ -2,5 +2,5 @@
 
 rm -rf docs/devel/phpdoc
 mkdir docs/devel/phpdoc
-phpdoc -ue on --output "HTML:Smarty:PHP" -d ./ -t docs/devel/phpdoc -ti "LDAP Account Manager - Documentation" -dc "LDAP Account Manager" -dn main
+phpdoc -d ./ -t docs/devel/phpdoc --title "LDAP Account Manager - Documentation" --defaultpackagename main -e php,inc
  
