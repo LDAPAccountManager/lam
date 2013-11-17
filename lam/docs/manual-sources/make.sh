d2e078c269e9678d347fd69009ec1b62a60f83e1
Roland Gruber <post@rolandgruber.de>
2013-11-17 10:17:33 +0000

    Bind DLZ update

--- lam/docs/manual-sources/make.sh
+++ lam/docs/manual-sources/make.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 # $Id$
 #
-# Copyright (C) 2009  Roland Gruber
+# Copyright (C) 2009 - 2013  Roland Gruber
 # This code is part of LDAP Account Manager (http://www.ldap-account-manager.org/)
 
 # This script is run to create the LAM manual.
@@ -11,7 +11,8 @@ rm -rf ../manual
 mkdir ../manual
 xsltproc -o ../manual/ --stringparam html.stylesheet.type text/css --stringparam html.stylesheet style.css /usr/share/xml/docbook/stylesheet/nwalsh/html/chunk.xsl howto.xml
 mkdir ../manual/images
-# cp images/*.jpg ../manual/images
 cp images/*.png ../manual/images
 cp images/*.jpg ../manual/images
+mkdir ../manual/resources
+cp resources/*.* ../manual/resources
 cp style.css ../manual
