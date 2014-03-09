c076ccb66bc6cb5687d919e6f143011f824b9035
Roland Gruber <post@rolandgruber.de>
2014-03-09 11:35:54 +0000

    generate single page HTML, too

--- lam/docs/manual-sources/make.sh
+++ lam/docs/manual-sources/make.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 # $Id$
 #
-# Copyright (C) 2009 - 2013  Roland Gruber
+# Copyright (C) 2009 - 2014  Roland Gruber
 # This code is part of LDAP Account Manager (http://www.ldap-account-manager.org/)
 
 # This script is run to create the LAM manual.
@@ -16,3 +16,13 @@ cp images/*.jpg ../manual/images
 mkdir ../manual/resources
 cp resources/*.* ../manual/resources
 cp style.css ../manual
+
+rm -rf ../manual-onePage
+mkdir ../manual-onePage
+xsltproc -o ../manual-onePage/ --stringparam html.stylesheet.type text/css --stringparam html.stylesheet style.css /usr/share/xml/docbook/stylesheet/nwalsh/html/onechunk.xsl howto.xml
+mkdir ../manual-onePage/images
+cp images/*.png ../manual-onePage/images
+cp images/*.jpg ../manual-onePage/images
+mkdir ../manual-onePage/resources
+cp resources/*.* ../manual-onePage/resources
+cp style.css ../manual-onePage
