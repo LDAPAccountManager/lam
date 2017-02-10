5274f7d048624e20b6f6c398114df43754088dee
Roland Gruber <post@rolandgruber.de>
2017-02-10 19:32:34 +0100

    xinclude

--- lam/docs/manual-sources/make.sh
+++ lam/docs/manual-sources/make.sh
@@ -8,7 +8,7 @@
 
 rm -rf ../manual
 mkdir ../manual
-xsltproc -o ../manual/ --stringparam html.stylesheet.type text/css --stringparam html.stylesheet style.css /usr/share/xml/docbook/stylesheet/nwalsh/html/chunk.xsl howto.xml
+xsltproc --xinclude -o ../manual/ --stringparam html.stylesheet.type text/css --stringparam html.stylesheet style.css /usr/share/xml/docbook/stylesheet/nwalsh/html/chunk.xsl howto.xml
 mkdir ../manual/images
 cp images/*.png ../manual/images
 mkdir ../manual/resources
--- lam/docs/manual-sources/makeOther.sh
+++ lam/docs/manual-sources/makeOther.sh
@@ -10,7 +10,7 @@ dir=`pwd`
 
 rm -rf ../manual-onePage
 mkdir ../manual-onePage
-xsltproc -o ../manual-onePage/ --stringparam html.stylesheet.type text/css --stringparam html.stylesheet style.css /usr/share/xml/docbook/stylesheet/nwalsh/html/onechunk.xsl howto.xml
+xsltproc --xinclude -o ../manual-onePage/ --stringparam html.stylesheet.type text/css --stringparam html.stylesheet style.css /usr/share/xml/docbook/stylesheet/nwalsh/html/onechunk.xsl howto.xml
 mkdir ../manual-onePage/images
 cp images/*.png ../manual-onePage/images
 mkdir ../manual-onePage/resources
@@ -19,7 +19,7 @@ cp style.css ../manual-onePage
 
 rm -rf ../manual-pdf
 mkdir ../manual-pdf
-xsltproc -o ../manual-pdf/out.fo --stringparam paper.type "A4" --stringparam generate.toc "book toc,title,table,figure" --stringparam "body.start.indent" "0pt" /usr/share/xml/docbook/stylesheet/nwalsh/fo/docbook.xsl howto.xml
+xsltproc --xinclude -o ../manual-pdf/out.fo --stringparam paper.type "A4" --stringparam generate.toc "book toc,title,table,figure" --stringparam "body.start.indent" "0pt" /usr/share/xml/docbook/stylesheet/nwalsh/fo/docbook.xsl howto.xml
 mkdir ../manual-pdf/images
 for img in `ls images/*.png`; do
 	convert -density 96 $img ../manual-pdf/$img
