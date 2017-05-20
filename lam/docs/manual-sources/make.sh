4e2fc18eda61a57539cba30caa5b7a3c02f158de
Roland Gruber <post@rolandgruber.de>
2017-05-20 09:13:36 +0200

    removed list of tables

--- lam/docs/manual-sources/make.sh
+++ lam/docs/manual-sources/make.sh
@@ -8,7 +8,7 @@
 
 rm -rf ../manual
 mkdir ../manual
-xsltproc --xinclude -o ../manual/ --stringparam html.stylesheet.type text/css --stringparam html.stylesheet style.css /usr/share/xml/docbook/stylesheet/nwalsh/html/chunk.xsl howto.xml
+xsltproc --xinclude -o ../manual/ --stringparam generate.toc "book toc" --stringparam html.stylesheet.type text/css --stringparam html.stylesheet style.css /usr/share/xml/docbook/stylesheet/nwalsh/html/chunk.xsl howto.xml
 mkdir ../manual/images
 cp images/*.png ../manual/images
 mkdir ../manual/resources
