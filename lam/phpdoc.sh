564fc2ed7f18b6579994ad71e72ca07c3ea3d582
Roland Gruber <post@rolandgruber.de>
2017-12-13 16:09:26 +0100

    docs

--- lam/phpdoc.sh
+++ lam/phpdoc.sh
@@ -2,4 +2,4 @@
 
 rm -rf docs/phpdoc
 mkdir docs/phpdoc
-phpdoc -d ./ -t docs/phpdoc --title "LDAP Account Manager" --template clean --defaultpackagename main -e php,inc --ignore lib/en*,lib/env.inc
+phpdoc -d ./ -t docs/phpdoc --title "LDAP Account Manager" --template clean --defaultpackagename main -e php,inc --ignore lib/en*,lib/env.inc,lib/3rdParty*,templates/3rdParty*
