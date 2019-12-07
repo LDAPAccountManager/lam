3d7e2e9c47b9766974b712efbfec540ecf93b269
Roland Gruber <post@rolandgruber.de>
2019-12-07 20:00:44 +0100

    copyright

--- lam-packaging/docker/start.sh
+++ lam-packaging/docker/start.sh
@@ -1,4 +1,24 @@
 #!/bin/bash
+#
+#  Docker start script for LDAP Account Manager
+
+#  This code is part of LDAP Account Manager (http://www.ldap-account-manager.org/)
+#  Copyright (C) 2019  Felix Bartels
+
+#  This program is free software; you can redistribute it and/or modify
+#  it under the terms of the GNU General Public License as published by
+#  the Free Software Foundation; either version 2 of the License, or
+#  (at your option) any later version.
+
+#  This program is distributed in the hope that it will be useful,
+#  but WITHOUT ANY WARRANTY; without even the implied warranty of
+#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#  GNU General Public License for more details.
+
+#  You should have received a copy of the GNU General Public License
+#  along with this program; if not, write to the Free Software
+#  Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+
 
 set -eu # unset variables are errors & non-zero return values exit the whole script
 [ "$DEBUG" ] && set -x
