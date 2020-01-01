ef9b3dd64ec928f92107ab51248c6f9140428f7b
Roland Gruber <post@rolandgruber.de>
2020-01-01 18:12:29 +0100

    webauthn

--- lam/tests/lib/2factorWebauthnTest.php
+++ lam/tests/lib/2factorWebauthnTest.php
@@ -37,7 +37,7 @@ class WebauthnProviderTest extends TestCase {
 	 */
 	private $config;
 
-	protected function setUp() {
+	protected function setUp(): void {
 		$this->config = new TwoFactorConfiguration();
 	}
 
