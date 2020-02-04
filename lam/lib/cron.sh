b15b94cb298e72ed263ab23d80e26446b9fd655e
Roland Gruber <post@rolandgruber.de>
2020-02-04 19:08:18 +0100

    PHP7

--- lam/lib/cron.sh
+++ lam/lib/cron.sh
@@ -2,10 +2,7 @@
 
 dir=`dirname $0`
 
-if [ -x /usr/bin/php5 ]; then
-	/usr/bin/php5 -f $dir/cron.inc $*
-	exit $?
-elif [ -x /usr/bin/php ]; then
+if [ -x /usr/bin/php ]; then
 	/usr/bin/php -f $dir/cron.inc $*
 	exit $?
 fi
