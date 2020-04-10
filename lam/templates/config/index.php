b65125beaf88f083ac3ac816d1048fa5c2ea5865
Roland Gruber <post@rolandgruber.de>
2020-04-10 20:34:36 +0200

    added dev mode

--- lam/templates/config/index.php
+++ lam/templates/config/index.php
@@ -2,7 +2,7 @@
 /*
 
   This code is part of LDAP Account Manager (http://www.ldap-account-manager.org/)
-  Copyright (C) 2003 - 2019  Roland Gruber
+  Copyright (C) 2003 - 2020  Roland Gruber
 
   This program is free software; you can redistribute it and/or modify
   it under the terms of the GNU General Public License as published by
