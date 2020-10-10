59e482c4098c0adabdcec03e9002aa9c22f64850
Roland Gruber <post@rolandgruber.de>
2020-10-10 14:59:58 +0200

    moved extended failed password messages to plugins

---
+++ lam/tests/lib/plugins/extendedInvalidCredentials/ExtraInvalidCredentialsTest.php
@@ -0,0 +1,108 @@
+<?php
+namespace LAM\PLUGINS\EXTRA_INVALID_CREDENTIALS;
+
+use DateInterval;
+use DateTime;
+use PHPUnit\Framework\TestCase;
+
+/*
+
+  This code is part of LDAP Account Manager (http://www.ldap-account-manager.org/)
+  Copyright (C) 2020  Roland Gruber
+
+  This program is free software; you can redistribute it and/or modify
+  it under the terms of the GNU General Public License as published by
+  the Free Software Foundation; either version 2 of the License, or
+  (at your option) any later version.
+
+  This program is distributed in the hope that it will be useful,
+  but WITHOUT ANY WARRANTY; without even the implied warranty of
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+  GNU General Public License for more details.
+
+  You should have received a copy of the GNU General Public License
+  along with this program; if not, write to the Free Software
+  Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+
+*/
+
+require_once __DIR__ . '/../../../../lib/modules.inc';
+require_once __DIR__ . '/../../../../lib/plugins/extendedInvalidCredentials/ExtraInvalidCredentials.inc';
+
+/**
+ * Checks the ExtraInvalidCredentials functionality.
+ *
+ * @author Roland Gruber
+ */
+class ExtraInvalidCredentialsTest extends TestCase {
+
+	/**
+	 * No attributes that indicate an issue -> no message.
+	 */
+	public function test_getExtraMessage_noMessage() {
+		$extraInvalidCredentials = $this
+			->getMockBuilder(ExtraInvalidCredentials::class)
+			->setMethods(array('getLdapData'))
+			->getMock();
+		$extraInvalidCredentials->method('getLdapData')->willReturn(array());
+
+		$this->assertNull($extraInvalidCredentials->getExtraMessage(null, 'testDn'));
+	}
+
+	/**
+	 * PPolicy issue.
+	 */
+	public function test_getExtraMessage_ppolicy() {
+		$extraInvalidCredentials = $this
+			->getMockBuilder(ExtraInvalidCredentials::class)
+			->setMethods(array('getLdapData'))
+			->getMock();
+		$extraInvalidCredentials->method('getLdapData')->willReturn(
+			array(
+				'pwdaccountlockedtime' => array('1234')
+			)
+		);
+
+		$this->assertNotNull($extraInvalidCredentials->getExtraMessage(null, 'testDn'));
+	}
+
+	/**
+	 * Kerberos password issue.
+	 */
+	public function test_getExtraMessage_mitKerberosPassword() {
+		$time = new DateTime('now', getTimeZone());
+		$time = $time->sub(new DateInterval('P1M'));
+		$extraInvalidCredentials = $this
+			->getMockBuilder(ExtraInvalidCredentials::class)
+			->setMethods(array('getLdapData'))
+			->getMock();
+		$extraInvalidCredentials->method('getLdapData')->willReturn(
+			array(
+				'krbpasswordexpiration' => array($time->format('YmdHis') . 'Z')
+			)
+		);
+
+		$this->assertNotNull($extraInvalidCredentials->getExtraMessage(null, 'testDn'));
+	}
+
+	/**
+	 * Kerberos account issue.
+	 */
+	public function test_getExtraMessage_mitKerberosAccount() {
+		$time = new DateTime('now', getTimeZone());
+		$time = $time->sub(new DateInterval('P1M'));
+		$extraInvalidCredentials = $this
+			->getMockBuilder(ExtraInvalidCredentials::class)
+			->setMethods(array('getLdapData'))
+			->getMock();
+		$extraInvalidCredentials->method('getLdapData')->willReturn(
+			array(
+				'krbprincipalexpiration' => array($time->format('YmdHis') . 'Z')
+			)
+		);
+
+		$this->assertNotNull($extraInvalidCredentials->getExtraMessage(null, 'testDn'));
+	}
+
+}
+
