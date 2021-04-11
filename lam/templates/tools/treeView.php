78f2b447818a367dddd0f512c2ce6fc7afa9793e
Roland Gruber <post@rolandgruber.de>
2021-04-11 17:03:15 +0200

    new tree view

---
+++ lam/templates/tools/treeView.php
@@ -0,0 +1,52 @@
+<?php
+namespace LAM\TOOLS\TREEVIEW;
+
+/*
+
+  This code is part of LDAP Account Manager (http://www.ldap-account-manager.org/)
+  Copyright (C) 2021  Roland Gruber
+
+  This program is free software; you can redistribute it and/or modify
+  it under the terms of the GNU General Public License as published by
+  the Free Software Foundation; either version 2 of the License, or
+  (at your option) any later version.
+
+  This program is distributed in the hope that it will be useful,
+  but WITHOUT ANY WARRANTY; without even the implied warranty of
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+  GNU General Public License for more details.
+
+  You should have received a copy of the GNU General Public License
+  along with this program; if not, write to the Free Software
+  Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+
+*/
+
+/**
+* LDAP tree view.
+*
+* @author Roland Gruber
+* @package tools
+*/
+
+/** security functions */
+include_once(__DIR__ . "/../../lib/security.inc");
+/** access to configuration options */
+include_once(__DIR__ . "/../../lib/config.inc");
+
+// start session
+startSecureSession();
+enforceUserIsLoggedIn();
+validateSecurityToken();
+
+checkIfToolIsActive('TreeViewTool');
+
+setlanguage();
+
+include __DIR__ . '/../../lib/adminHeader.inc';
+echo '<div class="user-bright smallPaddingContent">';
+
+
+
+echo '</div>';
+include __DIR__ . '/../../lib/adminFooter.inc';
