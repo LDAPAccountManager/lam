3623ee4d7442a55aa55803665e724f2fc7eac85e
Roland Gruber <post@rolandgruber.de>
2022-01-23 16:17:26 +0100

    support to import single self service profiles

--- lam/templates/config/confImportExport.php
+++ lam/templates/config/confImportExport.php
@@ -19,7 +19,7 @@ use ZipArchive;
 /*
 
   This code is part of LDAP Account Manager (http://www.ldap-account-manager.org/)
-  Copyright (C) 2020 - 2021  Roland Gruber
+  Copyright (C) 2020 - 2022  Roland Gruber
 
   This program is free software; you can redistribute it and/or modify
   it under the terms of the GNU General Public License as published by
