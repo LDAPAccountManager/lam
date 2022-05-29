88981d760a9c14c66bed9f768720ff70a009d7d5
Roland Gruber <post@rolandgruber.de>
2022-05-29 19:43:16 +0200

    new sortable implementation

--- codespell.sh
+++ codespell.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-~/.local/bin/codespell --skip '*3rdParty*,*/ckeditor/*,*/po/*,*/locale/*,tmp,sess,config,graphics,*/style/images/*,*/style/*.gif,*/style/*.png,*/docs/manual-onePage/*,*/docs/manual-sources/images/*,*/templates/lib/*jquery*,*/templates/lib/*flatpickr*,*~,*/docs/phpdoc/*,*/docs/manual/*,*/docs/devel/images/*,*/docs/manual-pdf/*,*.sh,*/cropper.js,*/lib/extra/*,lam/.phpdoc' --ignore-words-list "tim,te,pres,files'" lam
+~/.local/bin/codespell --skip '*3rdParty*,*/ckeditor/*,*/po/*,*/locale/*,tmp,sess,config,graphics,*/style/images/*,*/style/*.gif,*/style/*.png,*/docs/manual-onePage/*,*/docs/manual-sources/images/*,*/templates/lib/*jquery*,*/templates/lib/*flatpickr*,*/templates/lib/*Sortable*,*~,*/docs/phpdoc/*,*/docs/manual/*,*/docs/devel/images/*,*/docs/manual-pdf/*,*.sh,*/cropper.js,*/lib/extra/*,lam/.phpdoc' --ignore-words-list "tim,te,pres,files'" lam
