0d41af3c9fd2f509aa4ba130aab49d2ae17e2d47
Roland Gruber <post@rolandgruber.de>
2022-10-18 07:41:40 +0200

    #193 fixed debug check

--- lam-packaging/docker/start.sh
+++ lam-packaging/docker/start.sh
@@ -21,7 +21,7 @@
 
 
 set -eu # unset variables are errors & non-zero return values exit the whole script
-[ "$DEBUG" ] && set -x
+[[ -n $DEBUG ]] && [[ $DEBUG -eq "true" ]] && set -x
 
 if [ "${LAM_DISABLE_TLS_CHECK:-}" == "true" ]; then
   if ! grep -e '^TLS_REQCERT never$' /etc/ldap/ldap.conf > /dev/null; then
