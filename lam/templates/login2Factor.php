07645dee57767c75e387c5f1a7d6fd0d2d4de318
Roland Gruber <post@rolandgruber.de>
2023-02-27 16:58:58 +0100

    Okta refactoring

--- lam/templates/login2Factor.php
+++ lam/templates/login2Factor.php
@@ -101,9 +101,9 @@ if (empty($serials)) {
 	die();
 }
 
-if (isset($_POST['submit']) || isset($_POST['sig_response'])
-    || (isset($_GET['state']) && isset($_GET['code']))
-    || (isset($_GET['session_state']) && isset($_GET['redirect_uri']))) {
+if (isset($_POST['submit']) || isset($_POST['sig_response']) // WebAuthn
+    || (isset($_GET['state']) && isset($_GET['code'])) // Okta
+    || (isset($_GET['session_state']) && isset($_GET['redirect_uri']))) { // OpenID
 	$twoFactorInput = isset($_POST['2factor']) ? $_POST['2factor'] : null;
 	$serial = isset($_POST['serial']) ? $_POST['serial'] : null;
 	if (!$provider->hasCustomInputForm() && (empty($twoFactorInput) || !in_array($serial, $serials))) {
