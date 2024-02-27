08228f9f4bb639509fd6d3786cd4a07e7e061489
Roland Gruber <post@rolandgruber.de>
2024-02-27 19:40:32 +0100

    8.7

--- lam/docs/manual-sources/makeOther.sh
+++ lam/docs/manual-sources/makeOther.sh
@@ -8,15 +8,6 @@
 
 dir=`pwd`
 
-rm -rf ../manual-onePage
-mkdir ../manual-onePage
-xsltproc --xinclude -o ../manual-onePage/ --stringparam html.stylesheet.type text/css --stringparam html.stylesheet style.css /usr/share/xml/docbook/stylesheet/nwalsh/html/onechunk.xsl howto.xml
-mkdir ../manual-onePage/images
-cp images/*.png ../manual-onePage/images
-mkdir ../manual-onePage/resources
-cp resources/*.* ../manual-onePage/resources
-cp style.css ../manual-onePage
-
 rm -rf ../manual-pdf
 mkdir ../manual-pdf
 xsltproc --xinclude -o ../manual-pdf/out.fo --stringparam paper.type "A4" --stringparam generate.toc "book toc,title,table,figure" --stringparam "body.start.indent" "0pt" /usr/share/xml/docbook/stylesheet/nwalsh/fo/docbook.xsl howto.xml
