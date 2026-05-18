ee7d1e6fdb2a13497f2742cee58bbc0aa41dfe2d
Roland Gruber <post@rolandgruber.de>
2026-05-18 07:50:37 +0200

    deps

--- lam/lib/3rdParty/composer/composer/installed.php
+++ lam/lib/3rdParty/composer/composer/installed.php
@@ -326,9 +326,9 @@
             'dev_requirement' => false,
         ),
         'phpmailer/phpmailer' => array(
-            'pretty_version' => 'v7.0.2',
-            'version' => '7.0.2.0',
-            'reference' => 'ebf1655bd5b99b3f97e1a3ec0a69e5f4cd7ea088',
+            'pretty_version' => 'v7.1.0',
+            'version' => '7.1.0.0',
+            'reference' => 'e193b921a73e65bff40cab89ea0b0293be0e0c20',
             'type' => 'library',
             'install_path' => __DIR__ . '/../phpmailer/phpmailer',
             'aliases' => array(),
