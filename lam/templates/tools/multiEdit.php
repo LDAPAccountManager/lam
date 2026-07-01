9f6d378a160b30344890537c164909feb6ee5b14
Roland Gruber <post@rolandgruber.de>
2026-07-01 07:44:24 +0200

    #716 audit log

--- lam/templates/tools/multiEdit.php
+++ lam/templates/tools/multiEdit.php
@@ -521,7 +521,7 @@ function doModify(): array {
 		$changes = $_SESSION['multiEdit_status']['actions'][$dn];
 		$_SESSION['multiEdit_status']['modContent'] .= htmlspecialchars($dn) . "<br>";
 		// run LDAP commands
-		$success = ldap_modify($_SESSION['ldap']->server(), $dn, $changes);
+		$success = ldapModifyAttributes($_SESSION['ldap']->server(), $dn, $changes);
 		if (!$success || isset($_REQUEST['multiEdit_error'])) {
 			$msg = new htmlStatusMessage('ERROR', getDefaultLDAPErrorString($_SESSION['ldap']->server()));
 			$_SESSION['multiEdit_status']['modContent'] .= getMessageHTML($msg);
